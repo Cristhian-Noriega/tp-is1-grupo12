cb285b39516654d00ecce47debd64bd7c2f230b9
Cristhian-Noriega <noriegacristhian57@gmail.com>
2024-10-22 20:27:40 -0300

    Set up Docker

---
+++ order-app/src/test/java/is1/order_app/OrderAppApplicationTests.java
@@ -0,0 +1,14 @@
+package is1.order_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
