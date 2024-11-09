76e9d4c38a737573d65ddbd83ff7f1a2dfdfcae2
Cristhian-Noriega <noriegacristhian57@gmail.com>
2024-11-09 15:53:44 -0300

    User repository persistent

--- order-app/src/main/java/is1/order_app/repository/UserRepository.java
+++ order-app/src/main/java/is1/order_app/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package is1.order_app.repository;
 
 import is1.order_app.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository {
-    User save(User user);
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    List<User> findAll();
 }
 
